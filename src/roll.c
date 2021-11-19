4413b6efd70787240dc4f79a78b9fa9f0ff6145f
taniajunardi <73692955+taniajunardi@users.noreply.github.com>
2021-11-19 23:30:58 +0700

    Update roll.c

--- src/roll.c
+++ src/roll.c
@@ -4,56 +4,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
- int roll(Player P, Tele T){
- int dice1;
- srand ( time(NULL) );
- dice1 = (rand()%(P.MaxRoll) + 1);
- printf ("%s mendapatkan angka %d\n", P.Name,dice1);
-    if ((P.Curr+dice1 =='#') && (P.Curr-dice1=='#')){
-         printf ("%s tidak dapat bergerak", P.Name); }
-    else {
-        if ((P.Curr+dice1=='.') && (P.Curr-dice1=='.')){
-            printf ("%s dapat maju dan mundur\n", P.Name);
-            printf ("Ke mana %s mau bergerak : \n", P.Name);
-            printf ("1. %d\n", P.Curr-dice1);
-            printf ("2. %d\n", P.Curr+dice1);
-            int opsi;
-            printf("Masukkan pilihan :\n");
-            scanf ("%d", &opsi);
-            if (opsi==1){
-                printf ("%s mundur %d langkah\n", P.Name,dice1);
-                P.Curr = P.Curr-dice1;}
-            else if (opsi ==2){
-                printf ("%s maju %d langkah\n", P.Name,dice1);
-                P.Curr = P.Curr+dice1;}
-            printf ("%s berada di petak %d\n", P.Name,P.Curr);}
-            if (P.Curr != T.BeforeTele[i]){
-                printf("%s tidak menemukan teleporter", P.Name);}
-            else if (P.Curr == T.BeforeTele[i]){
-                printf("%s menemukan teleporter", P.Name);}//isi lagi nanti
-        else if (((P.Curr+dice1=='.') && (P.Curr-dice1=='#')) || ((P.Curr+dice1=='.') && (P.Curr-dice1 < 0)) ){
-            printf ("%s dapat maju\n", P.Name);
-            printf ("%s maju %d langkah\n", P.Name,dice1);
-            P.Curr = P.Curr+dice1;
-            printf ("%s berada di petak %d\n", P.Name,P.Curr); 
-            if (P.Curr != T.BeforeTele[i]){
-                printf("%s tidak menemukan teleporter", P.Name);}
-            else if (P.Curr == T.BeforeTele[i]){
-                printf("%s menemukan teleporter", P.Name);}//isi lagi nanti
-               
-        else if ((P.Curr+dice1=='#') && (P.Curr-dice1=='.')){
-            printf ("%s dapat mundur\n", P.Name);
-            printf ("%s mundur %d langkah\n",P.Name,dice1);
-            P.Curr = P.Curr-dice1;
-            printf ("%s berada di petak %d\n", P.Name,P.Curr);
-            if (P.Curr != T.BeforeTele[i]){
-                printf("%s tidak menemukan teleporter", P.Name);}
-            else if (P.Curr == T.BeforeTele[i]){
-                printf("%s menemukan teleporter", P.Name);}//isi lagi nanti
-        }
-    }            
+int roll(User P, Tele T){
+    int i;
+    int dice1;
+    srand ( time(NULL) );
+    if (skill==3){ //diganti lagi nanti
+        dice1 = (rand()%(P.MaxRoll-(P.MaxRoll/2)+1)) + (P.MaxRoll/2);
+    }
+    else if (skill==4){//diganti lagi nanti
+        dice1 = (rand()%(P.MaxRoll/2)) + 1;
+    }
+    else{
+        dice1 = (rand()%(P.MaxRoll)) + 1;}
+    printf ("%s mendapatkan angka %d.\n", P.Nama,dice1);
+        // tidak dapat bergerak
+        if ((P.Curr+dice1 =='#') && (P.Curr-dice1=='#')){
+            printf ("%s tidak dapat bergerak.\n", P.Nama); }
+        else {
+            //dapat gereak ke dua petak
+            if ((P.Curr+dice1=='.') && (P.Curr-dice1=='.')){
+                printf ("%s dapat maju dan mundur.\n", P.Nama);
+                printf ("Ke mana %s mau bergerak : \n", P.Nama);
+                printf ("1. %d\n", P.Curr-dice1);
+                printf ("2. %d\n", P.Curr+dice1);
+                int opsi;
+                printf("Masukkan pilihan :\n");
+                scanf ("%d", &opsi);
+                if (opsi==1){
+                    printf ("%s mundur %d langkah.\n", P.Nama,dice1);
+                    P.Curr = P.Curr-dice1;}
+                else if (opsi ==2){
+                    printf ("%s maju %d langkah.\n", P.Nama,dice1);
+                    P.Curr = P.Curr+dice1;}
+                printf ("%s berada di petak %d.\n", P.Nama,P.Curr);}
+                // tidak menemukan teleport
+                if (P.Curr != T.BeforeTele[i]){
+                    printf("%s tidak menemukan teleporter.\n", P.Nama);}
+                // ketemu teleport
+                else if (P.Curr == T.BeforeTele[i]){
+                    printf("%s menemukan teleporter.\n", P.Nama);}
+                    /*kalo ketemu imunitas teleport
+                    printf("%s memiliki imunitas teleport.\n", P.Nama);
+                    printf("Apakah %s ingin teleport (Y/N) ?\n", P.Nama);
+                    char pilihan;
+                    scanf ("%c", &pilihan);
+                    if (pilihan=="N"){
+                        printf("%s tidak teleport.\n", P.Nama);
+                        printf("Buff imunitas teleport hilang.\n");}
+                    else
+                        if (pilihan=="Y"){
+                            printf("%s teleport ke petak %d.\n", P.Nama, T.AfterTele[i];)}
+                    KALO G KETEMU IMUNITAS TELEPORT
+                    printf("%s tidak memiliki imunitas teleport.\n", P.Nama);
+                    printf("%s teleport ke petak %d.\n", P.Nama, T.AfterTele[i];  
+                        }}*/
+            //dapat bergerak ke satu petak (maju)
+            else if (((P.Curr+dice1=='.') && (P.Curr-dice1=='#')) || ((P.Curr+dice1=='.') && (P.Curr-dice1 < 0)) ){
+                printf ("%s dapat maju.\n", P.Nama);
+                printf ("%s maju %d langkah.\n", P.Nama,dice1);
+                P.Curr = P.Curr+dice1;
+                printf ("%s berada di petak %d.\n", P.Nama,P.Curr); 
+                if (P.Curr != T.BeforeTele[i]){
+                    printf("%s tidak menemukan teleporter", P.Nama);}
+                else if (P.Curr == T.BeforeTele[i]){
+                    printf("%s menemukan teleporter", P.Nama);}//isi lagi nanti
+            //dapat bergerak ke satu petak (mundur)  
+            else if ((P.Curr+dice1=='#') && (P.Curr-dice1=='.')){
+                printf ("%s dapat mundur\n", P.Nama);
+                printf ("%s mundur %d langkah\n",P.Nama,dice1);
+                P.Curr = P.Curr-dice1;
+                printf ("%s berada di petak %d\n", P.Nama,P.Curr);
+                if (P.Curr != T.BeforeTele[i]){
+                    printf("%s tidak menemukan teleporter", P.Nama);}
+                else if (P.Curr == T.BeforeTele[i]){
+                    printf("%s menemukan teleporter", P.Nama);}//isi lagi nanti
+            }
+        } }
     return P.Curr;
 }
-
-
 
