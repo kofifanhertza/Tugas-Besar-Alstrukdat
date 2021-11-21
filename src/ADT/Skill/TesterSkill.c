1ad717be7692f5719facb480c26c3e08aab16de2
Kofifan <kofifanhertzah@gmail.com>
2021-11-21 19:32:11 +0700

    Merapihkan skill

--- src/ADT/Skill/TesterSkill.c
+++ src/ADT/Skill/TesterSkill.c
@@ -18,59 +18,10 @@ int main () {
         } 
 
         else if (Input == 2) {
-           int inputCommand = CommandSkill(U) ;
-           if (inputCommand != 0) {
-                if (inputCommand > 0) {
 
-                    boolean use = true ;
+        U = SKILL(U) ;
 
-                    if (Search(U.ActiveSkill, IdxSkill(&U.SkillList, inputCommand)) != Nil) {
-                        printf("Gagal menggunakan, Skill sudah aktif!\n") ;
-                    }  else {
-
-                        if (IdxSkill(&U.SkillList, inputCommand) == 2) {
-                            if (NbElmt(U.SkillList) > 9) {
-                                printf("Cermin Pengganda Gagal Digunakan\n") ;
-                                use = false ;
-                            } else {
-                                printf("Skill Cermin Pengganda berhasil digunakan!\n") ;
-                                U.SkillList = SkillRandomizer(U) ;
-                                U.SkillList = SkillRandomizer(U) ;
-                                use = true ;
-                            }
-                        }
-                        if (IdxSkill(&U.SkillList, inputCommand) == 3) {
-                            if (Search(U.ActiveSkill, 4) != Nil) {
-                                printf("Gagal menggunakan skill, Skill Pembesar dan pengecil tidak bisa digunakan bersamaan!\n") ;
-                                use = false ;
-                            } 
-                        } else if (IdxSkill(&U.SkillList, inputCommand) == 4) {
-                            if (Search(U.ActiveSkill, 3) != Nil) {
-                                printf("Gagal menggunakan skill, Skill Pembesar dan pengecil tidak bisa digunakan bersamaan!\n") ;
-                                use = false ;
-                            } 
-                        }
-
-                        if (use == true) {
-                            U.ActiveSkill = UseSkill(U, inputCommand) ;
-                            U.SkillList = DelSkill(U, inputCommand) ;
-                        }
-                    }
-        
-
-                    
-               } else if (inputCommand < 0) {
-                    inputCommand = inputCommand * -1 ;
-                    DelIdxSkill (&U.SkillList, inputCommand) ;
-                    U.SkillList = DelSkill(U, inputCommand) ;
-               }
-               
-    
-           }
-           
-        }
-
-        else if (Input == 3) {
+        } else if (Input == 3) {
             int nSkill = NbElmt(U.SkillList) ;
             printf("%d\n", nSkill) ;
         }
--- src/ADT/Skill/listskill.c
+++ src/ADT/Skill/listskill.c
@@ -254,3 +254,54 @@ List SkillRandomizer (User U) {
     }
     return U.SkillList ;   
 }
+
+User SKILL (User U) {
+int inputCommand = CommandSkill(U) ;
+           if (inputCommand != 0) {
+                if (inputCommand > 0) {
+
+                    boolean use = true ;
+
+                    if (Search(U.ActiveSkill, IdxSkill(&U.SkillList, inputCommand)) != Nil) {
+                        printf("Gagal menggunakan, Skill sudah aktif!\n") ;
+                    }  else {
+
+                        if (IdxSkill(&U.SkillList, inputCommand) == 2) {
+                            if (NbElmt(U.SkillList) > 9) {
+                                printf("Cermin Pengganda Gagal Digunakan\n") ;
+                                use = false ;
+                            } else {
+                                printf("Skill Cermin Pengganda berhasil digunakan!\n") ;
+                                U.SkillList = SkillRandomizer(U) ;
+                                U.SkillList = SkillRandomizer(U) ;
+                                use = true ;
+                            }
+                        }
+                        if (IdxSkill(&U.SkillList, inputCommand) == 3) {
+                            if (Search(U.ActiveSkill, 4) != Nil) {
+                                printf("Gagal menggunakan skill, Skill Pembesar dan pengecil tidak bisa digunakan bersamaan!\n") ;
+                                use = false ;
+                            } 
+                        } else if (IdxSkill(&U.SkillList, inputCommand) == 4) {
+                            if (Search(U.ActiveSkill, 3) != Nil) {
+                                printf("Gagal menggunakan skill, Skill Pembesar dan pengecil tidak bisa digunakan bersamaan!\n") ;
+                                use = false ;
+                            } 
+                        }
+
+                        if (use == true) {
+                            U.ActiveSkill = UseSkill(U, inputCommand) ;
+                            U.SkillList = DelSkill(U, inputCommand) ;
+                        }
+                    }
+        
+
+                    
+               } else if (inputCommand < 0) {
+                    inputCommand = inputCommand * -1 ;
+                    DelIdxSkill (&U.SkillList, inputCommand) ;
+                    U.SkillList = DelSkill(U, inputCommand) ;
+               }
+           }
+    return U ;
+}
